6ea96e39256dc4a854fa6a2623153c62581f2a57
Unknown <wolfj@post.bgu.ac.il>
2017-12-04 22:50:13 +0200

    promise testGet()

--- src/test/java/bgu/spl/a2/PromiseTest.java
+++ src/test/java/bgu/spl/a2/PromiseTest.java
@@ -4,31 +4,59 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PromiseTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+//	@BeforeClass
+//	public static void setUpBeforeClass() throws Exception {
+//	}
+//
+//	@AfterClass
+//	public static void tearDownAfterClass() throws Exception {
+//	}
+	
+	private Promise<Integer> promise;
+	
+	
+	PromiseTest(){
+		promise = new Promise<>();
 	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		promise = new Promise<>();
 	}
 
 	@Test
 	public final void testGet() {
-		fail("Not yet implemented"); // TODO
+		try{
+			promise.get();
+			Assert.fail();	
+		} 
+		catch(IllegalStateException e) {
+			promise.resolve(100);
+			
+			try {
+				int i = promise.get();
+				Assert.assertEquals(i,100);
+			}
+			catch(Exception ex) {
+				Assert.fail();
+			}
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
 	}
 
 	@Test
