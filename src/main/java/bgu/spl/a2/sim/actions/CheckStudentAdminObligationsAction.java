7996abbfe3f29e7b79a92b820c4942f40082f21f
Unknown <wolfj@post.bgu.ac.il>
2017-12-13 18:19:44 +0200

    ...

--- src/main/java/bgu/spl/a2/sim/actions/CheckStudentAdminObligationsAction.java
+++ src/main/java/bgu/spl/a2/sim/actions/CheckStudentAdminObligationsAction.java
@@ -1,5 +1,8 @@
 package bgu.spl.a2.sim.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bgu.spl.a2.Action;
 import bgu.spl.a2.sim.Computer;
 import bgu.spl.a2.sim.privateStates.StudentPrivateState;
@@ -7,7 +10,7 @@ import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 class CheckStudentAdminObligationsAction extends Action<Boolean> {
 
 	private Computer Computer;
-	private String[] Conditions;
+	private List<String> Conditions;
 	private StudentPrivateState studentState;
 	
 	/**
@@ -16,19 +19,28 @@ class CheckStudentAdminObligationsAction extends Action<Boolean> {
 	 * @param conditions - list of courses student must pass
 	 */
 	public CheckStudentAdminObligationsAction(Computer computer, String[] conditions) {
-		this.Computer = computer;
-		Conditions = conditions;
+		setActionName("Administrative Check");
+		Computer = computer;
+		Conditions = new ArrayList<String>();
+		for(String course : conditions) {
+			Conditions.add(course);
+		}
 	}
 
 
-
+	/**
+	 * use provided computer to check the student's admin obligations
+	 */
 	@Override
 	protected void start() {
 		throwExceptionForInvalidActorStateType(StudentPrivateState.class);
 		studentState = (StudentPrivateState) getCurrentPrivateState();
 		
+		long sig = Computer.checkAndSign(Conditions, studentState.getGrades());
+		studentState.setSignature(sig);
+		studentState.addRecord(getActionName());
 		
-		
+		boolean success = (studentState.getSignature() == sig);
+		complete(success);
 	}
-
 }
