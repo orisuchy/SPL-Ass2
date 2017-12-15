5914181b30e348b9d6b7f396325986571a3890be
Unknown <wolfj@post.bgu.ac.il>
2017-12-15 17:28:35 +0200

    reworked CheckAdministrativeObligations

--- src/main/java/bgu/spl/a2/sim/actions/CheckAdministrativeObligations.java
+++ src/main/java/bgu/spl/a2/sim/actions/CheckAdministrativeObligations.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 import bgu.spl.a2.Action;
 import bgu.spl.a2.Promise;
+import bgu.spl.a2.callback;
 import bgu.spl.a2.sim.Computer;
+import bgu.spl.a2.sim.Simulator;
+import bgu.spl.a2.sim.SuspendingMutex;
+import bgu.spl.a2.sim.Warehouse;
 import bgu.spl.a2.sim.privateStates.DepartmentPrivateState;
 import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 
@@ -18,14 +22,6 @@ import bgu.spl.a2.sim.privateStates.StudentPrivateState;
  */
 
 class CheckAdministrativeObligations extends Action<Boolean> {
-
-	/* 
-	 * "Action" : "Administrative Check",
-		"Department": "CS",
-		"Students": ["123456789","5959595959"],
-		"Computer": "A",
-		"Conditions" : ["SPL", "Data Bases"]
-	 */
 	
 	private String[] Students;
 	private String[] Conditions;
@@ -48,42 +44,80 @@ class CheckAdministrativeObligations extends Action<Boolean> {
 
 
 	/**
-	 * Send action to retireve computer. Once computer is available, send the computer to 
-	 * 
+	 * Attempt to retrieve a computer from the warehouse.
+	 * Once the computer is obtained, send actions to all students to verify their admin obligation.
+	 * Once their admin obligations are checked - release the computer
 	 */
-	
-	//TODO - release computer!!!!
 	@Override
 	protected void start() {
-		//get computer
-		ArrayList<Action<Computer>> dependencies1 = new ArrayList<Action<Computer>>();
-		Action<Computer> getComputer = new GetComputerAction(Computer, Department);
-		dependencies1.add(getComputer);
-		sendMessage(getComputer, Department, new DepartmentPrivateState());
 		
-		then(dependencies1, ()->{
-			Computer receivedComputer = getComputer.getResult().get();
+		//Get computer promise from warehouse
+		Warehouse warehouse = Simulator.getWarehouse();
+		SuspendingMutex mutex = warehouse.getComputer(Computer);
+		Promise<Computer> promisedComputer = mutex.down();
+		
+		
+		if(promisedComputer.isResolved()) { //mutex was clear and immediately received computer
+			//Do phase1 and insert phase2 as "then" callback
+			ArrayList<Action<Boolean>> dependencies1 = phase1(promisedComputer);
+			then(dependencies1, () ->{
+				phase2(dependencies1, mutex);
+			});
+		}else { //computer is unavailable
+			//subscribe to the promise to get back into the pool
+			promisedComputer.subscribe(()->{
+				getActorThreadPool().submit(this, Department, getCurrentPrivateState());
+			});
 			
-			//create an action for each student that will verify their admin obligations
-			ArrayList<Action<Boolean>> dependencies2 = new  ArrayList<Action<Boolean>>();
-			for(String student : Students) {
-				Action<Boolean> checkStudent = 
-						new CheckStudentAdminObligationsAction(receivedComputer, Conditions);
-				dependencies2.add(checkStudent);
-				sendMessage(checkStudent, student, new StudentPrivateState());
+			//create empty dependency list for "then" and make "then" callback be phase1
+			ArrayList<Action<Computer>> dependencies0 = new ArrayList<Action<Computer>>(); //list is empty to allow immediate follow-up when calling action.handle again
+			then(dependencies0, ()->{
+				//Do phase1 and insert phase2 as "then" callback
+				ArrayList<Action<Boolean>> dependencies1 = phase1(promisedComputer);
+				then(dependencies1, () ->{
+					phase2(dependencies1, mutex);
+				});	
+			});
+		}
+	}
+	
+	
+	/**
+	 * Once a computer is obtained - create a "CheckStudentAdminObligationsAction"
+	 * for each student and send to the pool;
+	 * Call "then" to add these actions as the dependency list and add {@link phase2(ArrayList<Action<Boolean>>, SuspendingMutex)} as the "then" callback
+	 * @param promisedComputer
+	 * @return action dependency list for the next phase
+	 */
+	protected ArrayList<Action<Boolean>> phase1(Promise<Computer> promisedComputer) {
+		Computer receivedComputer = promisedComputer.get();
+		
+		//create an action for each student that will verify their admin obligations
+		ArrayList<Action<Boolean>> dependencies1 = new  ArrayList<Action<Boolean>>();
+		for(String student : Students) {
+			Action<Boolean> checkStudent = 
+					new CheckStudentAdminObligationsAction(receivedComputer, Conditions);
+			dependencies1.add(checkStudent);
+			sendMessage(checkStudent, student, new StudentPrivateState());
+		}
+		return dependencies1;
+	}
+	
+	/**
+	 * Verify that the students were checked for admin obligations.
+	 * Release the computer mutex once done.
+	 * @param dependencies1 - list of previous actions to check
+	 * @param mutex - once done, release this mutex
+	 */
+	protected void phase2(ArrayList<Action<Boolean>> dependencies1, SuspendingMutex mutex) {
+		boolean success = true;
+		for (Action<Boolean> action : dependencies1) {
+			if(!action.getResult().get()) {
+				success = false;
+				break;
 			}
-			
-			//make new dependency list on these "check student" actions
-			then(dependencies2, () ->{
-				boolean success = true;
-				for (Action<Boolean> action : dependencies2) {
-					if(!action.getResult().get()) {
-						success = false;
-						break;
-					}
-				}
-				complete(success);
-			});	
-		});
+		}
+		mutex.up(); //release mutex
+		complete(success);
 	}
 }
