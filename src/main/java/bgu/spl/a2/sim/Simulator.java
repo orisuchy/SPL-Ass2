a7c81b28c88ef06163a1379aa964239198a6cd38
Unknown <wolfj@post.bgu.ac.il>
2017-12-25 17:44:58 +0200

    added loop

--- src/main/java/bgu/spl/a2/sim/Simulator.java
+++ src/main/java/bgu/spl/a2/sim/Simulator.java
@@ -25,7 +25,7 @@ import bgu.spl.a2.sim.actions.SubmittableActionBoxFactory;
  */
 public class Simulator {
 
-	private static boolean DEBUG_MODE = false;
+	private static boolean DEBUG_MODE = true;
 	
 	private static String JSONinput;
 	private static ActorThreadPool actorThreadPool;
@@ -237,6 +237,10 @@ public class Simulator {
 	public static void main(String [] args){
 		String path = args[0];
 		
+		
+		for(int i=0; i<25000; i++) {
+			System.out.println(i);
+			
 		try {
 			JSONinput = readFile(path);
 		} catch (IOException e) {
@@ -256,5 +260,7 @@ public class Simulator {
 		attachWarehouse(newWarehouse);
 	
 		start();
+		System.out.println("end");
+		}
 	}
 }
